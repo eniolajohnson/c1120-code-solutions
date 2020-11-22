887fd35f8f81286ee80acba0275c209ee9661a6f
Eniola Johnson <writingincodes@gmail.com>
2020-11-22 16:16:16 +0100

    Completed array-manipulation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,6 +1,23 @@
 /* exported chunk */
-function chunk(array, size){
+// function chunk(array, size){
+//   var newArr = [...array];
+//   var nestedArr = arr.splice(size);
+//   console.log(nestedArr);
+// }
+
+function chunk(array, size) {
   var newArr = [...array];
-  var nestedArr = arr.splice(size);
-  console.log(nestedArr);
+  var nestedArr = [];
+  var returnedArr = []
+  nestedArr.push(newArr.slice(0, size));
+  nestedArr.push(newArr.slice(size, size + size));
+  nestedArr.push(newArr.slice(size + size, (size + size) + size));
+  nestedArr.push(newArr.slice((size + size) + size, (size + size) + (size + size)));
+  nestedArr.push(newArr.slice((size + size) + (size + size), (size + size) + (size + size) + size));
+  nestedArr.forEach(element => {
+    if (element.length !== 0) {
+      returnedArr.push(element)
+    }
+  });
+  return returnedArr;
 }
