7dbcc2af513d6fa8287cf9fe0a7113e0f62d915c
Eniola Johnson <writingincodes@gmail.com>
2020-11-30 21:26:44 +0100

    array-manipulation-3 WIP

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,4 +1,24 @@
 /* exported difference */
+function difference(first, second) {
+  var array = [...first, ...second];
+  var newArray = [... new Set(array)];
+  console.log(newArray);
+  var arr1 = [];
+  var arr2 = [];
+  for (var i = 0; i < array.length; i++) {
+    if (arr1.includes(array[i])) {
+      arr2.push(array[i]);
+    }
+    arr1.push(array[i]);
+  }
+  for (var i = 0; i < newArray.length; i++) {
+    if (newArray[i] == (arr2[i])) {
+      newArray.splice(i, 1);
+      i++;
+    }
+  }
+  return newArray;
+}
 // function difference(first, second) {
 //   var array = [...first, ...second];
 //   var arr1 = [];
@@ -9,5 +29,12 @@
 //     }
 //     arr1.push(array[i]);
 //   }
-//   return arr2.reverse();
+//   for (var i = 0; i < arr1.length; i++) {
+
+//     if (arr1[i].includes(arr2[i])) {
+//       arr1.splice(i, 1);
+//       i--;
+//     }
+//   }
+//   console.log(arr1);
 // }
