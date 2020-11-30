bca79ef514b1f6db3b0ff9e972af61d8f99232bd
Eniola Johnson <writingincodes@gmail.com>
2020-11-30 18:01:09 +0100

    array-manipulation-3 WIP

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,13 @@
 /* exported intersection */
+function intersection(first, second) {
+  var array = [...first, ...second];
+  var arr1 = [];
+  var arr2 = [];
+  for (var i = 0; i < array.length; i++) {
+    if (arr1.includes(array[i])) {
+      arr2.push(array[i]);
+    }
+    arr1.push(array[i]);
+  }
+  return arr2.reverse();
+}
