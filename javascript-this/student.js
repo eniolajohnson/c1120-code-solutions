2cc90e2ab4c6ff07ff909469c425d59469bf1178
Eniola Johnson <writingincodes@gmail.com>
2020-12-08 21:43:47 +0100

    Completed javascript-this

---
+++ javascript-this/student.js
@@ -0,0 +1,13 @@
+/* exported student */
+var student = {
+  firstName: 'Eniola',
+  lastName: 'Johnson',
+  subject: 'Javascript',
+  getFullName: function() {
+    return this.firstName + ' ' + this.lastName;
+  },
+  getIntroduction: function() {
+    var intro = 'Hello, my name is ' + this.firstName + ' ' + this.lastName + ' and I am studying ' + this.subject + '.';
+    return intro;
+  }
+}
