0b5d01f52a91851c9baae01195b97361660f1679
Eniola Johnson <writingincodes@gmail.com>
2020-12-30 19:08:49 +0100

    Added functionality to the backward and forward buttons

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,18 +1,39 @@
 var $pokemonImg = document.querySelectorAll('.hidden');
 var $defaultImg = document.querySelector('.default');
-var count = 5;
+var $backward = document.querySelector('button');
+var $forward = document.querySelector('#forward');
+
+var count = -1;
 var timerID = null;
+console.log($pokemonImg.length);
 
 function updateImage() {
-  count--;
-  if (count > -1){
+  count++;
+  if (count < 5){
      var itemAttribute = $pokemonImg[count].getAttribute('src');
      $defaultImg.setAttribute('src', itemAttribute);
-    // console.log($defaultImg.getAttribute('src'));
+    console.log($pokemonImg[count]);
+
+    $backward.addEventListener('click', function (e) {
+      itemAttribute = $pokemonImg[count - 1].getAttribute('src');
+      $defaultImg.setAttribute('src', itemAttribute);
+    })
+
+    $forward.addEventListener('click', function (e) {
+      itemAttribute = $pokemonImg[count + 1].getAttribute('src');
+      $defaultImg.setAttribute('src', itemAttribute);
+    })
     } else {
-    $defaultImg.setAttribute('src', './images/001.png');
       clearInterval(timerID);
     }
+  // console.log($pokemonImg[count - 1])
+  // console.log(itemAttribute);
 }
 
 timerID = setInterval(updateImage, 3000);
+
+//     // console.log($defaultImg.getAttribute('src'));
+// // console.log($pokemonImg[0].getAttribute('id'));
+// function moveForward(){
+//   var id = '0'
+// }
