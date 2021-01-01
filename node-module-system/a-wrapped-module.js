db2dff677f4472f8d67d79071ef5fefd2f5207a3
Eniola Johnson <writingincodes@gmail.com>
2021-01-01 01:25:36 +0100

    completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('The type of exports is', typeof exports);
+console.log('The value of exports is', exports);
+
+console.log('The type of require is', typeof require);
+console.log('The value of require is', require);
+
+console.log('The type of module is', typeof module);
+console.log('The value of module is', module);
+
+console.log('The type of __filename is', typeof __filename);
+console.log('The value of __filename is', __filename);
+
+console.log('The type of __dirname is', typeof __dirname);
+console.log('The value of __dirname is', __dirname);
