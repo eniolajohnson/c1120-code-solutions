7b7a3648d40bb2eedd11c76b55cc43ef94ad9f67
Eniola Johnson <writingincodes@gmail.com>
2021-01-22 20:41:40 +0100

    wip

---
+++ react-accordion-component/src/accordion.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// Hypertext Markup Language
+// Hypertext Markup Language(HTML) is the standard markup language for creating web pages and web applications.With Cascading Style Sheets(CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.
+
+// Cascading Style Sheets
+// Cascading Style Sheets(CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML.CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.
+
+// JavaScript
+// JavaScript, often abbreviated as JS, is a high - level, interpreted programming language that conforms to the ECMAScript specification JavaScript has curly bracket syntax, dynamic typing, prototype - based obiect - orientation, and first - class functions.
+
+
+const accordionInfo = [
+  {
+    title: Hypertext Markup Language
+  },
+  {
+    title: Cascading Style Sheets
+  },
+  {
+    title: JavaScript
+  }
+]
