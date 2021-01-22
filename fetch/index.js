3b8033fbc8b95b285fbdeee2e724efd425172f06
Eniola Johnson <writingincodes@gmail.com>
2021-01-22 22:34:15 +0100

    deleted test data

--- fetch/index.js
+++
@@ -1,8 +0,0 @@
-console.log('pet');
-
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(console.error('Fetch failed!'))
-
-fetch('https://pokeapi.co/api/v2/pokemon/1').then(response => response.json()).then(data => console.log(data)).catch(console.error('Fetch failed!'))
