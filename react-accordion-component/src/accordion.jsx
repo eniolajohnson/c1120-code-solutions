049a7738b0dbf10776276ff9a1a09615644b67e0
Eniola Johnson <writingincodes@gmail.com>
2021-03-15 15:29:38 +0100

    wip

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,19 +1,5 @@
 import React from 'react';
 
-const accordionInfo = [
-  {
-    title: 'Hypertext Markup Language',
-    description: 'Hypertext Markup Language(HTML) is the standard markup language for creating web pages and web applications.With Cascading Style Sheets(CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.'
-  },
-  {
-    title: 'Cascading Style Sheets',
-    description: 'Cascading Style Sheets(CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML.CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.'
-  },
-  {
-    title: 'JavaScript',
-    description: 'JavaScript, often abbreviated as JS, is a high - level, interpreted programming language that conforms to the ECMAScript specification JavaScript has curly bracket syntax, dynamic typing, prototype - based obiect - orientation, and first - class functions'
-  }
-];
 
 class CustomStructure extends React.Component {
   render() {
@@ -29,39 +15,35 @@ class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      html: false,
-      css: false,
-      js: false
+      index: 0,
     };
 
-    this.handleClickhtml = this.handleClickhtml.bind(this);
-    this.handleClickcss = this.handleClickcss.bind(this);
-    this.handleClickjs = this.handleClickjs.bind(this);
+    this.handleClick = this.handleClick;
   }
 
-  handleClickhtml() {
-    this.setState({
-      html: !this.state.html,
-      css: false,
-      js: false
-    });
-  }
+  // handleClickhtml() {
+  //   this.setState({
+  //     html: !this.state.html,
+  //     css: false,
+  //     js: false
+  //   });
+  // }
 
-  handleClickcss() {
-    this.setState({
-      html: false,
-      css: !this.state.css,
-      js: false
-    });
-  }
+  // handleClickcss() {
+  //   this.setState({
+  //     html: false,
+  //     css: !this.state.css,
+  //     js: false
+  //   });
+  // }
 
-  handleClickjs() {
-    this.setState({
-      html: false,
-      css: false,
-      js: !this.state.js
-    });
-  }
+  // handleClickjs() {
+  //   this.setState({
+  //     html: false,
+  //     css: false,
+  //     js: !this.state.js
+  //   });
+  // }
 
   render() {
     if (this.state.html === false && this.state.css === false && this.state.js === false) {
