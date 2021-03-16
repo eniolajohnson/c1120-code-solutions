22d4ad4965b44274c9b0060aa5f226a5d2735719
Eniola Johnson <writingincodes@gmail.com>
2021-03-16 17:07:27 +0100

    made my react-accordion project dynamic

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,184 +4,44 @@ class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      index: ''
+      index: null
     };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(e) {
-    {this.props.info.map(info => {
-      info.title === e.target.innerText
-      ? this.setState ({
-        index: info.index
-      })
-      : ''
+    {
+      this.props.info.map((info, i) => {
+        if (info.title === e.target.innerText){
+          this.setState({
+            index: i
+          })
+        }
+        if (this.state.index === i) {
+          this.setState({
+            index: null
+          })
+        }
     })}
   }
 
-  render () {
+  render() {
     const info = this.props.info;
-    return(
+    const index = this.state.index;
+    return (
       <div>
-        {info.map((detail) =>
+        {info.map((detail, i) =>
           <div>
             <h1 onClick={this.handleClick} className='headers'>{detail.title}</h1>
-            <p className='paragraphs'>{if (detail.index === this.state.index){
-                {detail.description}
-              }
-            }</p>
+            <p className={i === index ? 'paragraphs' : 'hide'}>
+              {detail.description}
+            </p>
           </div>
         )}
       </div>
     )
   }
 }
-
-
-// class Accordion extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       index: 0,
-//     };
-
-//     this.handleClick = this.handleClick;
-//   }
-
-//   // handleClickhtml() {
-//   //   this.setState({
-//   //     html: !this.state.html,
-//   //     css: false,
-//   //     js: false
-//   //   });
-//   // }
-
-//   // handleClickcss() {
-//   //   this.setState({
-//   //     html: false,
-//   //     css: !this.state.css,
-//   //     js: false
-//   //   });
-//   // }
-
-//   // handleClickjs() {
-//   //   this.setState({
-//   //     html: false,
-//   //     css: false,
-//   //     js: !this.state.js
-//   //   });
-//   // }
-
-//   render() {
-//     if (this.state.html === false && this.state.css === false && this.state.js === false) {
-//       return (
-//         <div>
-//           <div  onClick={this.handleClickhtml}>
-//             <CustomStructure text={accordionInfo[0].title} />
-//           </div>
-//           <div className='hide paragraphs'>
-//             <CustomStructure text={accordionInfo[0].description} />
-//           </div>
-
-//           <div className='headers' onClick={this.handleClickcss}>
-//             <CustomStructure text={accordionInfo[1].title} />
-//           </div>
-//           <div className='hide paragraphs'>
-//             <CustomStructure text={accordionInfo[1].description} />
-//           </div>
-
-//           <div className='headers' onClick={this.handleClickjs}>
-//             <CustomStructure text={accordionInfo[2].title} />
-//           </div>
-//           <div className='hide paragraphs'>
-//             <CustomStructure text={accordionInfo[2].description} />
-//           </div>
-//         </div>
-//       );
-//     }
-
-//     if (this.state.html === true && this.state.css === false && this.state.js === false) {
-//       return (
-//         <div>
-//           <div className='headers' onClick={this.handleClickhtml}>
-//             <CustomStructure text={accordionInfo[0].title} />
-//           </div>
-//           <div className='paragraphs'>
-//             <CustomStructure text={accordionInfo[0].description} />
-//           </div>
-
-//           <div className='headers' onClick={this.handleClickcss}>
-//             <CustomStructure text={accordionInfo[1].title} />
-//           </div>
-//           <div className='hide paragraphs'>
-//             <CustomStructure text={accordionInfo[1].description} />
-//           </div>
-
-//           <div className='headers' onClick={this.handleClickjs}>
-//             <CustomStructure text={accordionInfo[2].title} />
-//           </div>
-//           <div className='hide paragraphs'>
-//             <CustomStructure text={accordionInfo[2].description} />
-//           </div>
-//         </div>
-//       );
-//     }
-
-//     if (this.state.css === true && this.state.html === false && this.state.js === false) {
-//       return (
-//         <div>
-//           <div className='headers' onClick={this.handleClickhtml}>
-//             <CustomStructure text={accordionInfo[0].title} />
-//           </div>
-//           <div className='hide paragraphs'>
-//             <CustomStructure text={accordionInfo[0].description} />
-//           </div>
-
-//           <div className='headers' onClick={this.handleClickcss}>
-//             <CustomStructure text={accordionInfo[1].title} />
-//           </div>
-//           <div className='paragraphs'>
-//             <CustomStructure text={accordionInfo[1].description} />
-//           </div>
-
-//           <div className='headers' onClick={this.handleClickjs}>
-//             <CustomStructure text={accordionInfo[2].title} />
-//           </div>
-//           <div className='hide paragraphs'>
-//             <CustomStructure text={accordionInfo[2].description} />
-//           </div>
-//         </div>
-//       );
-//     }
-
-//     if (this.state.js === true && this.state.html === false && this.state.css === false) {
-//       return (
-//         <div>
-//           <div className='headers' onClick={this.handleClickhtml}>
-//             <CustomStructure text={accordionInfo[0].title} />
-//           </div>
-//           <div className='hide paragraphs'>
-//             <CustomStructure text={accordionInfo[0].description} />
-//           </div>
-
-//           <div className='headers' onClick={this.handleClickcss}>
-//             <CustomStructure text={accordionInfo[1].title} />
-//           </div>
-//           <div className='hide paragraphs'>
-//             <CustomStructure text={accordionInfo[1].description} />
-//           </div>
-
-//           <div className='headers' onClick={this.handleClickjs}>
-//             <CustomStructure text={accordionInfo[2].title} />
-//           </div>
-//           <div className='paragraphs'>
-//             <CustomStructure text={accordionInfo[2].description} />
-//           </div>
-//         </div>
-//       );
-//     }
-//   }
-// }
 
 export default Accordion;
